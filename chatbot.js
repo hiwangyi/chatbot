39081c0d2166f5a315691fe98d4635eeeeb01703
hiwangyi <wangyispaceman@gmail.com>
2023-03-08 09:52:56 +0800

    优化页面与交互

--- chatbot.js
+++ chatbot.js
@@ -1,14 +1,16 @@
 ;(function () {
   // Select elements
-  const chatbotContainer = document.querySelector(".chatbot-container")
   const chatbotMessages = document.querySelector(".chatbot-messages")
   const chatbotInput = document.querySelector(".chatbot-input-field")
   const chatbotSend = document.querySelector(".chatbot-send")
+  const loadingSpinner = document.getElementById("loading-spinner")
 
   const chatHistory = [] // Initialize an array to store previous messages
 
-  // Send message to chatbot
-  chatbotSend.addEventListener("click", () => {
+  const sendMessage = () => {
+    if (chatHistory.length >= 6) {
+      chatHistory.splice(0, chatHistory.length) // 对话达到3次则清空history，避免上下文中的token太多造成请求资源浪费
+    }
     const messageText = chatbotInput.value
     addMessage("user", messageText)
     chatbotInput.value = ""
@@ -18,22 +20,32 @@
     }
 
     // Make the API call with the payload
+    loadingSpinner.style.display = "block"
     fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization:
-          "Bearer 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",
+        Authorization: `Bearer ${MVP_CHATBOT_APIKEY}`,
       },
       body: JSON.stringify(payload),
     })
       .then((response) => response.json())
       .then((data) => {
         console.log(data)
+        loadingSpinner.style.display = "none"
         const message = data.choices[0].message.content
         addMessage("assistant", message)
       })
       .catch((error) => console.error(error))
+  }
+
+  // Send message to chatbot
+  chatbotSend.addEventListener("click", sendMessage)
+  chatbotInput.addEventListener("keyup", (e) => {
+    // Press Enter to send message
+    if (e.keyCode === 13) {
+      sendMessage()
+    }
   })
 
   // Add message to chatbot messages list
@@ -45,6 +57,10 @@
       sender === "user" ? message : processMarkdown(message)
     chatbotMessages.appendChild(messageElement)
     chatHistory.push({ role: sender, content: message }) // Add the message to chat history
+    chatbotMessages.scrollTo({
+      top: chatbotMessages.scrollHeight,
+      behavior: "smooth",
+    })
   }
 
   function processMarkdown(str) {
